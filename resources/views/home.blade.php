b035b57b987748d781ba2669bace5354493eca15
Humberto Viveros <humberto.viverosrios@gmail.com>
2023-10-20 16:26:41 -0300

    Inicio de proyecto, creación del template e instalación de dependencias

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.template')
+
+@section('titlePage', 'Agenda de contactos con Laravel')
+
+@section('content')
+    <h1>Hello world!</h1>
+@endsection
---
+++ resources/views/layout/template.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('titlePage')</title>
+
+    <!-- Bootstrap 5.3.2 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <!-- FontAwesome - Icons -->
+    <script src="https://kit.fontawesome.com/573783946a.js" crossorigin="anonymous"></script>
+
+    <!-- DataTable CSS + Bootstrap 5 -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+
+</head>
+
+<body>
+    
+    @yield('content')
+
+    <!-- Bootstrap 5.3.2 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <!-- JQuery 3.7.1 -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+    <!-- DataTable JS -->
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+
+    @yield('dataTable')
+
+</body>
+
+</html>
