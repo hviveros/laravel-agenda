330211ed748bf57d470c8ccddba8c9b04e230806
Humberto Viveros <humberto.viverosrios@gmail.com>
2023-10-20 17:18:39 -0300

    Diseño de vista home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,17 @@
 @section('titlePage', 'Agenda de contactos con Laravel')
 
 @section('content')
-    <h1>Hello world!</h1>
+
+<article class="py-5">
+    <h1>Agenda de contactos</h1>
+    <h3>Laravel 10.28.0</h3>
+
+    <div class="card" style="width: 18rem;">
+        <img src="https://placehold.co/600x400" class="card-img-top" alt="Home">
+        <div class="card-body">
+            <p class="card-text">Ejercicio para profundizar el aprendizaje del framework PHP Laravel. CRUD de categorías y contactos, base de datos MySQL, manejo de vistas SQL</p>
+        </div>
+    </div>
+</article>
+
 @endsection
--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -21,7 +21,38 @@
 
 <body>
     
-    @yield('content')
+    <!-- Menú principal -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="{{ route('home') }}"><i class="fas fa-id-card"></i></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('home') }}"><i class="fas fa-home"></i> Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fas fa-address-book"></i> Categorías</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fas fa-id-badge"></i> Contactos</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+
+        @yield('content')
+
+    </div>
+
+    <div class="footer py-4 my-4 border-top">
+      <p class="text-muted text-center"><small>2023 &copy; Créditos <a href="https://www.youtube.com/@FacultadAutodidacta" target="_blank">Facultad Autodidacta</a></small></p>
+    </div>
 
     <!-- Bootstrap 5.3.2 JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
